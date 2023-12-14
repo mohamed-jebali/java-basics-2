813f13b32d90767d3987c40312cded2c5aa22649
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-12-14 16:45:07 +0100

    javadoc method example done

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Java Seconda Repo Studio delle basi e delle fondamenta
+
+<p align="center">
+  <a href="https://docs.oracle.com/en/java/javase/21/javadoc/index.html#Java-Platform%2C-Standard-Edition">
+    <img src="https://www.vincenzoracca.com/images/java.png" height="250">
+  </a>
+</p>
+
+## Descrizione Repo
+
+Questa repository è dedicata allo studio dei concetti di base e delle fondamenta del linguaggio Java. Copre concetti essenziali e fornisce una base per ulteriori approfondimenti.
+
+## Javadoc
+
+JavaDoc è utilizzato in questo progetto per fornire una documentazione dettagliata del codice. Sono stati aggiunti commenti per spiegare lo scopo e l'uso dei diversi componenti.
+
+### Esempio:
+
+/**
+ * Questa classe rappresenta...
+ * ...
+ * @param paramName Descrizione del parametro.
+ * @return Descrizione del valore restituito.
+ */
+public class ClasseEsempio {
+    // Implementazione della classe...
+}
---
+++ src/App.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        App functionCheck = new App();
+
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+
+        functionCheck.checkOddEven(array);
+    }
+
+    /** La funzione verifica se i numeri all'interno dell'array sono pari o dispari
+     * <div>
+     * <img src="https://www.programmingsimplified.com/sites/default/files/odd-even.png" alt="Odd-Even Image">
+     * </div>
+     *
+     * 
+     * @param arrayExample argomento array numerico // commento con uso della libreria javadoc 
+     */
+
+    public void checkOddEven(int[] arrayExample){
+
+        for(int i = 0; i< arrayExample.length;i++){
+            if(arrayExample[i] % 2 == 0){
+                System.out.println(arrayExample[i] + " is even");
+            }
+            else{
+                System.out.println(arrayExample[i] + " is odd");
+            }
+        }
+    }
+}
